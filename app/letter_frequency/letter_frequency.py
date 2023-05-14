c8e1e1329f47476623c29fa188f9a21268dc5891
Petr Janik <peta.janik@email.cz>
2023-05-14 18:39:13 +0200

    Fix letter_frequency.py

--- app/letter_frequency/letter_frequency.py
+++ app/letter_frequency/letter_frequency.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel
 
 
-def count_letter_frequency(text: str) -> dict:
+def count_letter_frequency(text: str) -> dict[str, int]:
     return {letter: text.count(letter) for letter in set(text)}
 
 
-def reduce_letter_frequency(results: list[dict[str, int]]) -> dict:
-    return {k: sum([d[k] for d in results]) for k in set().union(*results)}
+def reduce_letter_frequency(results: list[dict[str, int]]) -> dict[str, int]:
+    def get_or_default(dictionary: dict[str, int], key: str) -> int:
+        return dictionary.get(key, 0)
+
+    return {k: sum([get_or_default(d, k) for d in results]) for k in set().union(*results)}
 
 
 class LetterFrequencyModel(BaseModel):
